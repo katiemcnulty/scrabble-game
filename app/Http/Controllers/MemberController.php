59c87e5baf98fe086d1a51753ea0de252495d3b6
katiemcnulty <ktmcnulty94@gmail.com>
2021-11-07 22:07:22 +0000

    formatted controller

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
 use App\Models\Member;
-
 
 class MemberController extends Controller
 {
@@ -19,35 +17,32 @@ class MemberController extends Controller
         $members = Member::orderBy('created_at', 'ASC')->get();
 
         
-
-        
-        foreach($members as $member ) {
+        foreach ($members as $member) {
 
             //add to each score for each game
             $score = 0;
             $totalGames = 0;
             $winner = 0;
-            $loser = 0;    
+            $loser = 0;
             $highestScore = 0;
             $highestScoringGame = false;
             $highestScoringPlayer = '';
                              
             //calculating player1 and player2 games
-            foreach($member->player1Games as $game) {
+            foreach ($member->player1Games as $game) {
                 $score += $game->player1_score;
 
                 //calculate winner or loser
-                if($game->player1_score > $game->player2_score) {
+                if ($game->player1_score > $game->player2_score) {
                     $winner++;
                 } else {
                     $loser++;
                 }
 
                 //calculate highest score
-                if ($game->player1_score > $highestScore){
+                if ($game->player1_score > $highestScore) {
                     $highestScore = $game->player1_score;
                     $highestScoringGame = true;
-                    
                 } else {
                     $highestScoringGame = 'No Game Data';
                 }
@@ -56,19 +51,19 @@ class MemberController extends Controller
                 $totalGames++;
             }
 
-            foreach($member->player2Games as $game) {
+            foreach ($member->player2Games as $game) {
                 $score += $game->player2_score;
 
                 //calculate winner or loser
 
-                if($game->player2_score > $game->player1_score) {
+                if ($game->player2_score > $game->player1_score) {
                     $winner++;
                 } else {
                     $loser++;
-                }               
+                }
 
                 //calculate highest score
-                if ($game->player2_score > $highestScore){
+                if ($game->player2_score > $highestScore) {
                     $highestScore = $game->player2_score;
                     $highestScoringGame = true;
                 } else {
@@ -80,27 +75,25 @@ class MemberController extends Controller
 
             //calculating average score
 
-            if($totalGames===0 || $score === 0){
+            if ($totalGames===0 || $score === 0) {
                 $memberAvg = 0;
-
-            }else {
-                
+            } else {
                 $memberAvg =  $score/$totalGames;
             }
 
             //adding time of game for highest score
             if ($highestScoringGame) {
-                $bestGameTime = date("Y-d-m H:i",strtotime($game->date_played));
+                $bestGameTime = date("Y-d-m H:i", strtotime($game->date_played));
             } else {
                 $bestGameTime = 'No Game Data';
-            } 
+            }
 
             $member->bestGameTime = $bestGameTime;
             $member->highScore = $highestScore;
             $member->bestPlayer = $highestScoringPlayer;
             $member->wins = $winner;
             $member->loses = $loser;
-            $member->averageScore= round($memberAvg,2);
+            $member->averageScore= round($memberAvg, 2);
         }
 
         return $members;
@@ -120,7 +113,7 @@ class MemberController extends Controller
     {
         $existingMember = Member::find($id);
 
-        if( $existingMember) {
+        if ($existingMember) {
             $existingMember->name = $request->get('member')['name'];
             $existingMember->email = $request->get('member')['email'];
 
@@ -129,7 +122,6 @@ class MemberController extends Controller
         }
 
         return "Member not found";
-
     }
     public function destroy($id)
     {
